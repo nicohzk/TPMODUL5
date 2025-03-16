c00b9148e265ab161acee7e16610b58bda00aa95
nicohzk <nicohzk05@gmail.com>
2025-03-16 16:44:58 +0700

    menambahkan class HaloGeneric dengan method

--- tpmodul5_103022300127/Program.cs
+++ tpmodul5_103022300127/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿class Halogeneric {
+    public void SapaUser<T>(T User) {
+        Console.WriteLine("Halo "+ User);
+    }
+}
+class Program
+{
+    static void Main(string[] args)
+    {
+        Halogeneric halo = new Halogeneric();
+        string namaPraktikan = "Hizkia";
+        halo.SapaUser(namaPraktikan);
+    }
+}
